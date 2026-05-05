02f7e2d29cd6e5db8823715a2bd5c57c64c96b22
Ava Heath Williams <126925721+Ava-HW@users.noreply.github.com>
2026-05-06 10:21:08 +1200

    Added database connection

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
 from flask import Flask, render_template
 from flask import request, redirect, url_for
+from functools import wraps
+from flask import g, redirect
+import sqlite3
 
 app = Flask(__name__)
 
 # to activate venv: .\.venv\Scripts\Activate.ps1
+
+# code to connect database to flask app
+DATABASE = 'hsk.db'
+app.secret_key = 'the random string'
+
+def make_dicts(cursor, row):
+    return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
+
+# code to start database connection
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    db.row_factory = make_dicts
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
  
 @app.route("/")
 def hello_world():
+    db = get_db()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM words;")
+    thing = cur.fetchall()
+    print(thing)
     return render_template("index.html")
 
 @app.route("/sign_in", methods = ["POST", "GET"])
