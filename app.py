7159d86aa1dc978f88830e5a53eff58f9c216ee2
Ava Heath Williams <126925721+Ava-HW@users.noreply.github.com>
2026-05-26 09:03:57 +1200

    Added ability to sign in (can't sign out yet)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask import request, redirect, url_for
 from functools import wraps
 from flask import g, redirect
@@ -6,6 +6,15 @@ import sqlite3
 import csv
 
 app = Flask(__name__)
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # Check if user ID is in session
+        if 'user_id' not in session:
+            return redirect(url_for('sign_in', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
 
 # to activate venv: .\.venv\Scripts\Activate.ps1
 
@@ -39,11 +48,20 @@ def hello_world():
 
 @app.route("/sign_in", methods = ["POST", "GET"])
 def sign_in():
+    db = get_db()
+    cur = db.cursor()
     if request.method == "POST":
         email = request.form.get('email')
         password = request.form.get('password')
         # check if email and password match
-        return redirect(url_for("user_home"))
+        query = "SELECT * FROM users WHERE email = ?;"
+        data = (email,)
+        cur.execute(query, data)
+        result = cur.fetchone()
+        if result:
+            if(result['password'] == password):
+                session['user_id'] = email
+                return redirect(url_for("user_home"))
     return render_template("sign_in.html")
 
 @app.route("/sign_up", methods = ["POST", "GET"])
@@ -61,9 +79,11 @@ def sign_up():
     return render_template("sign_up.html")
 
 @app.route("/user_home", methods = ["POST", "GET"])
+@login_required
 def user_home():
     db = get_db()
     cur = db.cursor()
+    # get list of words to display
     cur.execute("SELECT * FROM words;")
     words = cur.fetchall()
     return render_template("user_home.html", words = words)
